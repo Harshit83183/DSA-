99a96669007c54179ee1433242d3f5cc9b342945
Harshit83183 <harshittiwari964841@gmail.com>
2026-08-21 00:17:30 +0530

    commit

--- Functions/Fun1.cpp
+++ Functions/Fun1.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int power(int a, int b){
     int ans = 1;
-    for(int i = 0; i <=b; i++){
+    for(int i = 0; i < b; i++){
         ans = ans * a;
     }
+    return ans;
 }
 int main (){
     int a, b;
+    cout << "Enter Numbers : ";
     cin >> a >> b;
-    power(a, b);
+    cout << "The Answer is : " << power(a,b);
     return 0;
 }
