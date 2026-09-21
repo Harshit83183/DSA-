46b0ad68dbe9f14cecda54fd2df615fab4163070
Harshit83183 <harshittiwari964841@gmail.com>
2026-09-21 22:41:08 +0530

    Array

--- Array/Updating_Elements.cpp
+++ Array/Updating_Elements.cpp
@@ -24,7 +24,7 @@ void update(int arr[], int size){
 
     //displaying updated array ->
     for(int i = 0; i < size; i++){
-        cout << arr[i] << " ";
+        cout <<  arr[i] << " ";
     }
     cout << endl;
 }
